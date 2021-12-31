ebce749d07ee24fb3e4b73769b71136b4f8d11a0
Hemavathi12 <hemavathyamsk@gmail.com>
2021-12-31 03:49:27 -0800

    login

--- src/org/com/Login.java
+++ src/org/com/Login.java
@@ -1,5 +1,12 @@
 package org.com;
 
 public class Login {
+		public static void password() {
+				System.out.println("login");
 
+		}
+		public static void main(String[] args) {
+				password();
+		
+		}
 }
